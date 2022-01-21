547a510dbc0a26c855b175d6b1003236896009e4
naufaldi <naufaldi.rafif@gmail.com>
2022-01-22 00:12:38 +0700

    add: integrated eslint & prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,7 +19,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
-import { render } from 'react-dom';
-import 'styles/style.css';
-import App from 'components/App';
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import 'styles/style.css'
+import App from 'components/App'
 
 render(
   <StrictMode>
     <App />
   </StrictMode>,
   document.getElementById('root')
-);
+)
