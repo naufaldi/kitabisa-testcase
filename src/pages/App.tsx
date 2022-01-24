77516ab604b78c36392ce914fc69b363144f06e7
naufaldi <naufaldi.rafif@gmail.com>
2022-01-25 00:34:31 +0700

    feat: add seo

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import Container from 'components/common/Container'
 import Grid from 'components/common/Grid'
+import SeoComponent from 'components/common/SeoComponent'
 import Card from 'components/homepage/Card'
 import Filter from 'components/homepage/Filter'
 import ListCard from 'components/homepage/ListCard'
@@ -7,23 +8,26 @@ import Layout from 'components/layout/Layout'
 
 function App() {
   return (
-    <Layout>
-      <Container>
-        <Grid>
-          <div className="col-span-2 lg:col-span-3 flex items-center justify-center">
-            <img
-              src="/src/public/kitabisa.png"
-              className="h-14 lg:h-20 w-auto mr-auto lg:mx-auto"
-              alt="Logo Kita Bisa"
-            />
-          </div>
-          <Filter />
-        </Grid>
-        <Grid className="mt-12">
-          <ListCard />
-        </Grid>
-      </Container>
-    </Layout>
+    <>
+      <SeoComponent templateTitle="Kitabisa Homepage" />
+      <Layout>
+        <Container>
+          <Grid>
+            <div className="col-span-2 lg:col-span-3 flex items-center justify-center">
+              <img
+                src="/src/public/kitabisa.png"
+                className="h-14 lg:h-20 w-auto mr-auto lg:mx-auto"
+                alt="Logo Kita Bisa"
+              />
+            </div>
+            <Filter />
+          </Grid>
+          <Grid className="mt-12">
+            <ListCard />
+          </Grid>
+        </Container>
+      </Layout>
+    </>
   )
 }
 
